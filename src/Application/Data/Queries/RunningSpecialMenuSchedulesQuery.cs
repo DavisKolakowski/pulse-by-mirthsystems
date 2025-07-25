0283ba52481681c7332a2c835e9be250dc540ea0
Davis Kolakowski <DavisKolakowski@users.noreply.github.com>
2025-07-25 08:06:19 -0400

    Enhance ApplicationDbContext and add query classes
    Updated ApplicationDbContext to include new DbSet properties for various entities and enhanced the OnModelCreating method with PostgreSQL extensions. Overrode SaveChanges and SaveChangesAsync to manage timestamps using an injected clock service.
    
    Introduced RunningSpecialMenuSchedulesQuery with a SearchDateTime property for querying special menu schedules. Added SpecialMenuScheduleSearchQuery to support searching with properties for area, term, and category IDs, along with sorting options.

---
+++ src/Application/Data/Queries/RunningSpecialMenuSchedulesQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NodaTime;
+
+namespace Application.Data.Queries;
+public class RunningSpecialMenuSchedulesQuery
+{
+    public LocalDateTime SearchDateTime { get; set; }
+}
