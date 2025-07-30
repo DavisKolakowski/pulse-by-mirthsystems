732ad04a929cdd327eec530f9568a696c7cc42c9
Davis Kolakowski <DavisKolakowski@users.noreply.github.com>
2025-07-30 14:39:55 -0400

    Update dependencies and configure Hangfire integration
    - Updated `Aspire.Keycloak.Authentication` to a newer version.
    - Added `Aspire.Azure.Npgsql.EntityFrameworkCore.PostgreSQL` and `Hangfire.PostgreSql` packages in `Application.API.csproj`.
    - Replaced `Hangfire.PostgreSql` with `Hangfire.AspNetCore` in `Application.csproj`.
    - Enhanced `Program.cs` to configure Azure PostgreSQL and set up Hangfire for background jobs.
    - Integrated Hangfire dashboard into the HTTP request pipeline.

--- src/Application.API/Program.cs
+++ src/Application.API/Program.cs
@@ -2,8 +2,16 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 using Application.API.Authorization.Handlers;
+using Application.Contracts.Services;
+using Application.Infrastructure.Data;
+using Application.Infrastructure.Services;
+
+using Hangfire;
+using Hangfire.PostgreSql;
 
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 using NodaTime;
 using NodaTime.Serialization.SystemTextJson;
@@ -17,7 +25,31 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        builder.Services.AddSingleton<IAuthorizationHandler, VenueAuthorizationHandler>();
+        builder.AddAzureNpgsqlDbContext<ApplicationDbContext>("application-db", configureDbContextOptions: options =>
+        {
+            options.UseNpgsql(npgsqlOptions =>
+            {
+                npgsqlOptions.UseNodaTime();
+                npgsqlOptions.UseNetTopologySuite();
+            })
+            .UseSnakeCaseNamingConvention();
+
+            #if DEBUG
+            options.EnableSensitiveDataLogging();
+            options.EnableDetailedErrors();
+            #endif
+        });
+
+        builder.Services.AddHangfire(config =>
+        {
+            config.UsePostgreSqlStorage(c =>
+            {
+                c.UseNpgsqlConnection(builder.Configuration.GetConnectionString("application-db"));
+            });
+        });
+
+        builder.Services.AddScoped<ISpecialMenuScheduler, SpecialMenuScheduler>();
+
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
@@ -46,6 +78,8 @@ public class Program
             options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
         });
 
+        builder.Services.AddSingleton<IAuthorizationHandler, VenueAuthorizationHandler>();
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -58,8 +92,9 @@ public class Program
 
         app.UseAuthorization();
 
-
         app.MapControllers();
+
+        app.UseHangfireDashboard();
 
         app.Run();
     }
